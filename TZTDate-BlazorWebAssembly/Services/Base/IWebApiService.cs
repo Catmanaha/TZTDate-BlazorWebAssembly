58648a413b7a42b3d1dfc956007cca732cead7ce
miltim7 <timursailing70@gmail.com>
2024-04-29 02:29:19 +0400

    Add Account

--- TZTDate-BlazorWebAssembly/Services/Base/IWebApiService.cs
+++ TZTDate-BlazorWebAssembly/Services/Base/IWebApiService.cs
@@ -7,9 +7,10 @@ namespace TZTDateBlazorWebAssembly.Services.Base;
 public interface IWebApiService
 {
     public Task Register(UserRegisterDto userRegisterDto);
-    public Task Login(UserLoginDto loginDto);
+    public Task<string> Login(UserLoginDto loginDto);
     public Task<ProfilesDto> GetProfiles(string userId, string searchByName, int? startAge, int? endAge, string interests, string searchGender);
     public Task<string> GetAccountData(int userId);
     public Task<string> GetUserIdFromJwt();
     public Task<DateUserAndRecomendations> GetRecomendationsAsync();
+    public Task<string> UpdateUsernameAsync(string newUsername, string newDescription);
 }
