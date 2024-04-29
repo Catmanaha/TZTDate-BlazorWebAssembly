f8bf91b6c48da36efff8c78a01941a548654427d
Catmanaha <dadashov.tamerlan@gmail.com>
2024-04-29 18:17:41 +0400

    Fix chat button

--- TZTDate-BlazorWebAssembly/Components/DetailsBase.razor.cs
+++ TZTDate-BlazorWebAssembly/Components/DetailsBase.razor.cs
@@ -5,6 +5,7 @@ using TZTDate_BlazorWebAssembly.Dtos;
 using TZTDate_BlazorWebAssembly.Models;
 using TZTDateBlazorWebAssembly.Services.Base;
 using TZTDateBlazorWebAssembly.Models.Zodiac.Entities;
+using Microsoft.AspNetCore.SignalR.Client;
 
 
 namespace TZTDateBlazorWebAssembly.Components;
@@ -26,22 +27,22 @@ public class DetailsBase : ComponentBase
     [Inject]
     private IWebApiService webApiService { get; set; }
     [Inject]
-    private ILocalStorageService localStorageService { get; set; }
-    [Inject]
     private NavigationManager Navigation { get; set; }
 
     protected override async Task OnInitializedAsync()
     {
+
+
         this.UserDetails = await GetDetails();
         this.CurrentUser = this.UserDetails.CurrentUser;
         this.ViewedUser = this.UserDetails.ViewedUser;
         this.ProfilePicPaths = this.UserDetails.ImageUris ?? new List<string>();
         this.ZodiacCompatibility = await CurrentUser.CompatibilityByZodiacSign(ViewedUser);
         if (CurrentUser.Followed is not null) LikeButtonState = CurrentUser.Followed.Any(user => user.Id == ViewedUser.Id);
-        if (ViewedUser.Followed is not null && CurrentUser.Followed is not null) ReplicateMembership = CurrentUser.Followed.Any(user => user.Id == ViewedUser.Id)
-                                    && ViewedUser.Followed.Any(user => user.Id == CurrentUser.Id);
-        else ReplicateMembership = false;
-        System.Console.WriteLine(ProfilePicPaths.Count());
+        ReplicateMembership = ViewedUser.Followed != null && CurrentUser.Followed != null
+                      && CurrentUser.Followers.Any(user => user.Id == ViewedUser.Id)
+                      && CurrentUser.Followed.Any(user => user.Id == ViewedUser.Id);
+        System.Console.WriteLine(ReplicateMembership);
     }
 
     private async Task<UserDetailsDto> GetDetails()
@@ -57,14 +58,20 @@ public class DetailsBase : ComponentBase
         LikeButtonState = !LikeButtonState;
         if (LikeButtonState)
         {
-            // await JSRuntime.InvokeVoidAsync("alert", "Liked!");
             await webApiService.SetMembership(CurrentUser.Id, ViewedUser.Id);
+
         }
         else
         {
-            // await JSRuntime.InvokeVoidAsync("alert", "Unliked!");
             await webApiService.SetMembership(CurrentUser.Id, ViewedUser.Id);
+
         }
+
+        ReplicateMembership = ViewedUser.Followed != null && CurrentUser.Followed != null
+                          && CurrentUser.Followers.Any(user => user.Id == ViewedUser.Id)
+                          && CurrentUser.Followed.Any(user => user.Id == ViewedUser.Id);
+
+        StateHasChanged();
     }
 
     protected async Task ChatButtonClick()
